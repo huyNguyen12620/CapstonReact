8b2117106555efafaeadae8e87523182a27b2757
huynguyen <huynguyen1262000@gmail.com>
2022-09-27 00:18:24 +0700

    setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import { Outlet } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
+  <a className="navbar-brand" href="#">Navbar</a>
+  <button className="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId" aria-expanded="false" aria-label="Toggle navigation" />
+  <div className="collapse navbar-collapse" id="collapsibleNavId">
+    <ul className="navbar-nav me-auto mt-2 mt-lg-0">
+      <li className="nav-item">
+        <a className="nav-link active" href="#" aria-current="page">Home <span className="visually-hidden">(current)</span></a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#">Link</a>
+      </li>
+      <li className="nav-item dropdown">
+        <a className="nav-link dropdown-toggle" href="#" id="dropdownId" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+        <div className="dropdown-menu" aria-labelledby="dropdownId">
+          <a className="dropdown-item" href="#">Action 1</a>
+          <a className="dropdown-item" href="#">Action 2</a>
+        </div>
+      </li>
+    </ul>
+    <form className="d-flex my-2 my-lg-0">
+      <input className="form-control me-sm-2" type="text" placeholder="Search" />
+      <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+
+     
+     <Outlet/>
     </div>
   );
 }
