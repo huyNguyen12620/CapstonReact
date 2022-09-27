2e64c4984c40b0dcdbb7c9a9224275fdad601a31
huynguyen <huynguyen1262000@gmail.com>
2022-09-27 23:05:20 +0700

    capston cont

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, Routes ,Route} from 'react-router-dom';
+import index from './pages/Index/Index';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
       <BrowserRouter>
              <Routes>
               <Route path='' element={<App/>}></Route>
+              <Route path='/index' element={<index/>}></Route>
              </Routes>
       </BrowserRouter>
 );
